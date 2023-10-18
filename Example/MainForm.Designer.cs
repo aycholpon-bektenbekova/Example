d028f75874797c40f84c16789d20e49b89d3c65d
aycholpon-bektenbekova <aicholpon25t@gmail.com>
2023-10-18 22:20:10 +0600

    tic tac toe

--- Example/MainForm.Designer.cs
+++ Example/MainForm.Designer.cs
@@ -29,9 +29,20 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            restart = new Button();
+            label2 = new Label();
+            button9 = new Button();
+            button8 = new Button();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
             panel2 = new Panel();
-            label1 = new Label();
             Closebutton = new Button();
+            label1 = new Label();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
             SuspendLayout();
@@ -39,12 +50,133 @@
             // panel1
             // 
             panel1.BackColor = Color.Maroon;
+            panel1.Controls.Add(restart);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(button9);
+            panel1.Controls.Add(button8);
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
             panel1.Controls.Add(panel2);
             panel1.Dock = DockStyle.Fill;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
             panel1.Size = new Size(800, 450);
             panel1.TabIndex = 1;
+            // 
+            // restart
+            // 
+            restart.Location = new Point(629, 224);
+            restart.Name = "restart";
+            restart.Size = new Size(94, 68);
+            restart.TabIndex = 11;
+            restart.Text = "Начать заново";
+            restart.UseVisualStyleBackColor = true;
+            restart.Click += restart_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.White;
+            label2.Location = new Point(629, 159);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 10;
+            label2.Text = "label2";
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            button9.Location = new Point(505, 352);
+            button9.Name = "button9";
+            button9.Size = new Size(94, 77);
+            button9.TabIndex = 9;
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button1_Click;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            button8.Location = new Point(353, 352);
+            button8.Name = "button8";
+            button8.Size = new Size(94, 77);
+            button8.TabIndex = 8;
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button1_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            button7.Location = new Point(205, 352);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 77);
+            button7.TabIndex = 7;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button1_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.Location = new Point(505, 252);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 77);
+            button6.TabIndex = 6;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button1_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Location = new Point(353, 252);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 77);
+            button5.TabIndex = 5;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button1_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(205, 252);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 77);
+            button4.TabIndex = 4;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(505, 159);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 77);
+            button3.TabIndex = 3;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(353, 159);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 77);
+            button2.TabIndex = 2;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button1_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(205, 159);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 77);
+            button1.TabIndex = 1;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // panel2
             // 
@@ -57,16 +189,6 @@
             panel2.Size = new Size(800, 129);
             panel2.TabIndex = 0;
             // 
-            // label1
-            // 
-            label1.Font = new Font("Showcard Gothic", 28.2F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(256, 34);
-            label1.Name = "label1";
-            label1.Size = new Size(244, 59);
-            label1.TabIndex = 0;
-            label1.Text = "main ";
-            label1.TextAlign = ContentAlignment.MiddleCenter;
-            // 
             // Closebutton
             // 
             Closebutton.BackColor = Color.FromArgb(255, 255, 128);
@@ -77,6 +199,16 @@
             Closebutton.Text = "x";
             Closebutton.UseVisualStyleBackColor = false;
             Closebutton.Click += Closebutton_Click;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Showcard Gothic", 28.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(256, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(244, 59);
+            label1.TabIndex = 0;
+            label1.Text = "main ";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // MainForm
             // 
@@ -89,6 +221,7 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "MainForm";
             panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             panel2.ResumeLayout(false);
             ResumeLayout(false);
         }
@@ -99,5 +232,16 @@
         private Panel panel2;
         private Label label1;
         private Button Closebutton;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button9;
+        private Label label2;
+        private Button restart;
     }
 }
